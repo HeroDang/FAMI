ab26e764d74676494389dd2d2a4716366818ac5d
DangHung <danghungabt.bte@gmail.com>
2023-01-04 17:44:09 +0700

    dung base frontend project

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,26 @@
+//Layout
+import { HeaderOnly } from '@/components/Layout';
+//Pages
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Profile from '@/pages/Profile';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/following',
+        component: Following,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+        layout: HeaderOnly,
+    },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
