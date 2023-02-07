13bcda558daed2d1ab78e79ccaa45ba07d3bb337
Hùng Đặng Quốc <20520194@ms.uit.edu.vn>
2023-02-07 08:20:31 +0000

    Deleted backend

--- backend/routes/accountRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import {getAccount} from '../controllers/accountController.js'
-
-
-router.route('/').get(getAccount)
-
-
-export default router
--- backend/routes/personRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-import {getPersons} from '../controllers/personController.js'
-
-
-router.route('/').get(getPersons)
-
-
-export default router
